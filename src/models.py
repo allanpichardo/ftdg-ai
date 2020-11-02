decbd08ae56aeb0a5c8b2d10e7f8abd48ba3bcf4
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2020-11-01 22:42:11 -0800

    Flattening instead of avg pooling

--- src/models.py
+++ src/models.py
@@ -96,13 +96,14 @@ def get_2d_encoder():
 
 def get_efficientnet_triplet(sr=22050, duration=8.0, embedding_size=128):
     i = get_audio_layer(sr, duration)
-    en = tf.keras.applications.efficientnet.EfficientNetB0(include_top=False, input_shape=(341, 128, 3), pooling='max', weights='imagenet')
+    en = tf.keras.applications.efficientnet.EfficientNetB0(include_top=False, input_shape=(341, 128, 3), weights='imagenet')
     model = tf.keras.Sequential([
         i,
         tf.keras.layers.LayerNormalization(),
         tf.keras.layers.Conv2D(3, (3, 3), padding='same'),
         en,
-        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(4000, activation='relu'),
         tf.keras.layers.Dense(embedding_size, activation=None),
         tf.keras.layers.Lambda(lambda x: tf.math.l2_normalize(x, axis=1)),  # L2 normalize embeddings,
     ])
