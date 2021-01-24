ef58d15fde81542695523390f3edf7f0b6ee157f
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-01-24 01:33:40 -0800

    Using cosine similarity for results

--- server.py
+++ server.py
@@ -100,11 +100,10 @@ def get_first_neighbor(embedding, origins):
     for i in range(1, 97):
         query = query + "cube_ll_coord(embedding, {}) * {}{}".format(i, embedding[i-1], " + " if i < 96 else "")
     query = query + ") as cosine_distance from public.music where origin in %s order by cosine_distance asc limit 1"
-    print(query)
 
     cursor.execute(
         query,
-        (tuple(origins), embedding))
+        (tuple(origins),))
     results = cursor.fetchone()
     next_origin = results[5]
     next_embedding = list(eval(results[1]))
